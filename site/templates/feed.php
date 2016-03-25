ed6794a2029a4d1536588063234c94b50ad9414a
Lukas Bestle <mail@lukasbestle.com>
2016-03-25 12:40:45 +0100

    RSS feed for the changelog

--- site/templates/feed.php
+++ site/templates/feed.php
@@ -1,7 +1,9 @@
 <?php
 
-echo page('blog')->children()->visible()->flip()->limit(10)->feed(array(
+$feedPage = $page->page()->or('blog');
+
+echo page($feedPage)->children()->visible()->flip()->limit(10)->feed(array(
   'title'       => $page->title(),
   'description' => $page->description(),
-  'link'        => 'blog',
+  'link'        => $feedPage,
 ));
