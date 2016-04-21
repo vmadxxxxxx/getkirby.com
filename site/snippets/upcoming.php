6af0b4945036a567934922498eede4f5c6f1485c
Lukas Bestle <mail@lukasbestle.com>
2016-04-21 11:43:27 +0200

    Fix typo in upcoming info

--- site/snippets/upcoming.php
+++ site/snippets/upcoming.php
@@ -1,6 +1,6 @@
 <?php if($page->since()->future()): ?>
 <div class="info">
 This is an upcoming feature for version <?php echo $page->since()->version() ?>. The feature and this docs might still be under active development.
-Don't rely on it in a life project so far.
+Don't rely on it in a public project so far.
 </div>
 <?php endif ?>
