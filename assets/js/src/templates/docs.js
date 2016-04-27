65c47b73a6163f732a36f3c8ac0de8d6a3943772
Bastian Allgeier <mail@bastianallgeier.com>
2016-04-27 17:23:49 +0200

    Fixed the tocs

--- assets/js/src/templates/docs.js
+++
@@ -1,7 +0,0 @@
-// Add section anchors to every <h2> + <h3> within docs
-
-$('.text h2, .text h3').each(function() {
-    var anchor = $(this).text().replace(/[\-\[\]\/\{\}\(\)\*\+\?\!\.\,\=\\\^\$\'\&\%\|]/g, '').replace(/\s/g,'-');
-    $(this).attr('id',anchor.toLowerCase());
-  }
-);
