1c213cd8aad0e895a06642c99c2c5947b6ae9963
Bastian Allgeier <mail@bastianallgeier.com>
2016-06-30 13:02:41 +0200

    Kirby Kosmos Form

--- site/templates/home.php
+++ site/templates/home.php
@@ -93,7 +93,7 @@
     </ul>
   </section>
 
-  <section class="connect section last">
+  <section class="connect section">
     <h2 class="beta">Connect</h2>
     <ul class="list-2"><!--
    --><li>
@@ -115,6 +115,27 @@
         </a>
       </li><!--
  --></ul>
+  </section>
+
+  <section class="newsletter section last" id="kosmos">
+    <h2 class="beta">Kirby Kosmos</h2>
+
+    <p>Get monthly updates about new versions, plugins, themes, community events and more…</p>
+
+    <form action="http://getkirby.createsend.com/t/t/s/xdildi/" method="post" id="subForm">
+      <p>
+        <label for="fieldEmail">Email</label>
+        <input id="fieldEmail" name="cm-xdildi-xdildi" type="email" placeholder="mail@example.com" required />
+      </p>
+      <p>
+        <label for="fieldName">Name</label>
+        <input id="fieldName" name="cm-name" type="text" />
+      </p>
+      <p>
+        <button type="submit">Subscribe</button>
+      </p>
+    </form>
+
   </section>
 
 </main>
