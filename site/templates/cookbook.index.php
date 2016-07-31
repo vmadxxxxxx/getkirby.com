3d0a353139bf4956349def842e030b9531c6f1c9
texnixe <sonja@imap.cc>
2016-07-31 14:10:41 +0200

    add category filter

--- site/templates/cookbook.index.php
+++ site/templates/cookbook.index.php
@@ -2,19 +2,21 @@
 
 <main class="main" role="main">
 
-  <ul class="docs-index-list list-3">
-    <?php foreach($page->children()->visible() as $item): ?><!--
- --><li>
-      <div class="text">
-        <h2 class="delta"><a href="<?php echo $item->url() ?>"><?php echo html($item->title()) ?></a></h2>
-        <p class="text small"><? e($item->category()->isNotEmpty(), 'Category: '. $item->category()->html(), '') ?></p>
+  <div class="col-4-6 last">
+    <ul class="docs-index-list list-2">
+      <?php foreach($items as $item): ?><!--
+   --><li>
+        <div class="text">
+          <h2 class="delta"><a href="<?php echo $item->url() ?>"><?php echo html($item->title()) ?></a></h2>
+          <p class="text small"><? e($item->category()->isNotEmpty(), 'Category: '. $item->category()->html(), '') ?></p>
 
-        <?php echo kirbytext($item->description()) ?>
-      </div>
-    </li><!--
- --><?php endforeach ?>
-</ul>
-
+          <?php echo kirbytext($item->description()) ?>
+        </div>
+     </li><!--
+    --><?php endforeach ?>
+    </ul>
+  </div>
+  <?php snippet('category-filter', array('categories' => $categories)) ?>
 </main>
 
 <?php snippet('footer') ?>
