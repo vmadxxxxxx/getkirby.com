41f26729879e529b45c30eaa19bac996b2f6a376
Bastian Allgeier <mail@bastianallgeier.com>
2016-08-05 09:41:05 +0200

    Small improvements to the filter list

--- site/snippets/category-filter.php
+++ site/snippets/category-filter.php
@@ -1,9 +1,11 @@
 <div class="submenu filter col-2-6">
-  <h2 class="delta">Filter by:</h2>
-  <a class="button" href="<?= $page->url() ?>">x Remove filter</a>
+  <h2 class="delta">Filter by</h2>
+  <?php if(param('category')): ?>
+  <a class="button" href="<?= $page->url() ?>"><span style="color: #999; font-size: 1.1em; top: 2px; position: relative; padding-right: .25em">&times;</span> remove filter</a>
+  <?php endif ?>
   <ul>
     <li>
-      <a<?php e(param('category') == 'new', ' class="is-active"', '')?> href="<?= $page->url() . '/category:new' ?>"><?= 'NEW' ?></a>
+      <a<?php e(param('category') == 'new', ' class="is-active"', '')?> href="<?= $page->url() . '/category:new' ?>"><?= 'new' ?></a>
     </li>
     <?php foreach($categories as $category): ?>
       <li>
